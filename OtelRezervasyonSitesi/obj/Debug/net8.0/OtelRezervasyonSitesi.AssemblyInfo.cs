049956c6b3d95a42b0792cb0638c741a223a4350
ZEYNEP NURİYE TEKİN <zeynep.tekin@istun.edu.tr>
2024-07-05 14:05:30 +0300

    deneme

--- OtelRezervasyonSitesi/obj/Debug/net8.0/OtelRezervasyonSitesi.AssemblyInfo.cs
+++ OtelRezervasyonSitesi/obj/Debug/net8.0/OtelRezervasyonSitesi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtelRezervasyonSitesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f398cb6137660598ba61a3e9bc60d19a4672be46")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtelRezervasyonSitesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtelRezervasyonSitesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
