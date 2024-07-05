f398cb6137660598ba61a3e9bc60d19a4672be46
ZEYNEP NURİYE TEKİN <zeynep.tekin@istun.edu.tr>
2024-07-05 13:58:46 +0300

    Add project files

---
+++ OtelRezervasyonSitesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OtelRezervasyonSitesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
