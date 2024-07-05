4f4f251f6a434967a0e218cba1db9bc026042746
ZEYNEP NURİYE TEKİN <zeynep.tekin@istun.edu.tr>
2024-07-05 15:17:45 +0300

    şablon ayarlaniyor

--- OtelRezervasyonSitesi/Program.cs
+++ OtelRezervasyonSitesi/Program.cs
@@ -9,12 +9,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(); // Bu sat?r, wwwroot dizinindeki statik dosyalar? kullanman?za olanak sa?lar.
 
 app.UseRouting();
 
